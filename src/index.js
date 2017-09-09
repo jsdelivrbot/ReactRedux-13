6a1c3711893a21ea744973b25655fd35fabde2dc
Shohin Khamidjanov <skhamidjanov@verisign.com>
2017-09-09 19:22:22 -0400

    Youtube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const API_KEY = 'AIzaSyBQ4i1dsF1h3hVRfTTFr-fUIeD9uCI-UHE';
+
 const App = () => {
 	return <div>Hello, world!</div>;
 }
