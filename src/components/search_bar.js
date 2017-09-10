55fe92d264ac794dbb3b89c1c3de32761acdfdcd
Shohin Khamidjanov <shohin26@gmail.com>
2017-09-10 14:24:48 -0400

    YT API

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => <input />;
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    onChange={event => this.setState({term: event.target.value})}
+                    value={this.state.term}
+                />
+				Value of the input: {this.state.term}
+            </div>
+        );
+    }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBQ4i1dsF1h3hVRfTTFr-fUIeD9uCI-UHE';
+YTSearch({key: API_KEY, term: 'wtf'}, data => {
+    console.log(data);
+});
 
-const App = () => <div>
-		Hello, world!
-</div>;
-
+const App = () => (
+    <div><SearchBar /></div>
+);
 ReactDOM.render(<App />, document.getElementById('app'));
