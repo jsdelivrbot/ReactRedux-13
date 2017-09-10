e4b784a5678bb2388613919a3b39308d4d061c26
Shohin Khamidjanov <shohin26@gmail.com>
2017-09-10 15:08:46 -0400

    Some react stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBQ4i1dsF1h3hVRfTTFr-fUIeD9uCI-UHE';
-YTSearch({key: API_KEY, term: 'wtf'}, data => {
-    console.log(data);
-});
 
-const App = () => (
-    <div><SearchBar /></div>
-);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'wtf'}, videos => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.getElementById('app'));
