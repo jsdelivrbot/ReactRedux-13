eca62566a70220c2a4d6b4352cd5f894178c37fb
Shohin Khamidjanov <shohin26@gmail.com>
2017-09-10 13:38:56 -0400

    Eslint config. Searchbar component init:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBQ4i1dsF1h3hVRfTTFr-fUIeD9uCI-UHE';
 
-const App = () => {
-	return <div>Hello, world!</div>;
-}
+const App = () => <div>
+		Hello, world!
+</div>;
 
 ReactDOM.render(<App />, document.getElementById('app'));
